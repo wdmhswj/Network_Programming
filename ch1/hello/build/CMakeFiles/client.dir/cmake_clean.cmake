81749638a3b282108402ba4721c08052f0aeb52e
Opposolis <wmsjshihao@gmail.com>
2024-07-26 14:00:20 +0800

    first commit

---
+++ ch1/hello/build/CMakeFiles/client.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../bin/client"
+  "../bin/client.pdb"
+  "CMakeFiles/client.dir/hello_client.c.o"
+  "CMakeFiles/client.dir/hello_client.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
